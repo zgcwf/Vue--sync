3475060de0d80bc0563a1d7c576e57b9f7f368cb
zgcwf <1935083614@qq.com>
2022-02-16 20:21:52 +0800

    sync语法

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    App.vue
+     我是 {{doc.name}}
+     今年 {{doc.age}}
+
+     我现在有 {{total}}
+
+    <hr>
+    简写--语法糖
+    <Child :money.sync="total"/>
+    <Child  v-bind.sync="doc"/>
+    <hr>
+    正常写法
+    <!-- <Child :money="total" @update:money="total = $event" /> -->
+    <Child :money="total" @update:money="changeTotal($event)" />
+    <Child :name="doc.name" :age="doc.age" @update:age="doc.age = $events" />
+
+    <!-- <Child :name="doc.name" :age="doc.age" @update:age="changeAge($event)" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Child from "./Child.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return { 
+      total: 10000,
+      doc:{
+        name:"zgc",
+        age:18
+        }
+     };
+  },
+  components: { Child: Child },
+  methods:{
+    changeTotal(events){
+      this.total = events
+    },
+     changeAge(events){
+      this.doc.age = events
+    },
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app {
+  border: 3px solid red;
+  padding: 10px;
 }
 </style>
---
+++ src/Child.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="child">
+    {{ money }}
+    {{ name }}
+    {{ age }}
+    <button @click="changeMoney">
+      <span>花钱</span>
+    </button>
+    <button @click="changeAge">
+      <span>成长</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["money","name","age"],
+  methods: {
+    changeMoney() {
+      this.$emit("update:money", this.money - 100);
+    },
+     changeAge() {
+      this.$emit("update:age", this.age + 1);
+    },
+  },
+};
+</script>
+
+
+<style>
+.child {
+  border: 3px solid green;
+}
+</style>
